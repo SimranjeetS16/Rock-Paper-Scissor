f1d76c420ef836b02b76664bf38b22ad526d2043
SimranjeetS16 <singh.shergill1601@gmail.com>
2022-11-10 21:58:08 +0530

    functioning

--- rps.js
+++ rps.js
@@ -3,67 +3,87 @@ let paperbtn = document.querySelector(".paper");
 let scissorbtn = document.querySelector(".scissor");
 let playeroption = [rockbtn,paperbtn,scissorbtn];
 let computeroption = ["rock","paper","scissor"]
-let playercount = 00;
-let compcount = 00;
+let playercount = 0;
+let compcount = 0;
 
 playeroption.forEach(option => {
  option.addEventListener('click',function(){
         playeroption = option.innerHTML;
         let choicenumber = Math.floor(Math.random()*3);
         computerchoice = computeroption[choicenumber];
-        // computeroption = computeroption.toLowerCase();
         console.log(computerchoice);
         playeroption = playeroption.toLowerCase();
         console.log(playeroption);
+        key1 = "You Win";
+        key2 = "Computer Win";
 
         if(playeroption === computerchoice){
             alert("It is a 'Tie'");
+            key = "Match Tie!"
+            result(key);
         }
         else if(playeroption == "rock"){
             if(computerchoice == "scissor"){
-                alert("You win the game")
+               
                 playercount++;
                 playerwin();
+                result(key1);
             }
             else{
-                alert("Computer is the Winner")
+               
                 compcount++;
                 compwin();
+                result(key2);
             }
         
         }
         else if(playeroption == "paper"){
             if(computerchoice == "rock"){
-                alert("You Win the game")
+               
                 playercount++;
                 playerwin();
+                result(key1);
             }
             else{
-                alert("Computer is the Winner")
+               
                 compcount++;
                 compwin();
+                result(key2);
             }
         }
         else if(playeroption == "scissor"){
             if(computerchoice == "paper"){
-                alert("You Win the game")
+               
                 playercount++;
                 playerwin();
+                result(key1);
             }
             else{
-                alert("Computer is the Winner")
+               
                 compcount++;
                 compwin();
+                result(key2);
             }
         }
+        computerMove(computerchoice);
+        playerMove(playeroption);
         console.log(playercount);
         console.log(compcount);
  })
 })
 
 function playerwin(){
-    document.querySelector(".player_win").innerHTML = playercount;
+    document.querySelector(".player-score").innerHTML = playercount;
 }
 function compwin(){
-    document.querySelector(".comp_win").innerHTML = compcount;
+    document.querySelector(".comp-score").innerHTML = compcount;
+}
+function result(key){
+    document.querySelector(".result").innerHTML = key;
+}
+function playerMove(argument){
+    document.querySelector(".player_move").innerHTML = argument;
+}
+function computerMove(argument){
+    document.querySelector(".comp_move").innerHTML = argument;
 }
